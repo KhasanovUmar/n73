c8c30226c88cb6980a9a00645c1d0d083dcfa01a
Khasanov <hasanovumarbek921@gmail.com>
2025-11-26 00:32:43 +0500

    1

--- carproject.py
+++ carproject.py
@@ -79,7 +79,7 @@ class Avtosalon:
         sarlavha = "XODIMLAR ROYXATI" if tur == 'xodim' else "MIJOZLAR ROYXATI"
 
         if not royxat:
-            print(f"ℹ{sarlavha} bo'sh.")
+            print(f"{sarlavha} bo'sh.")
             return
 
         print(f"\n--- {sarlavha} ---")
